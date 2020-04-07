6c3e3b380ee866086b3bfa66c5254182974f8b33
Jaya Shankar Vadisela <jvadisela@innominds.com>
2020-04-08 03:00:41 +0530

    integrated Swagger

--- gau-flask-ml/gau/app.py
+++ gau-flask-ml/gau/app.py
@@ -1,11 +1,43 @@
 from flask import Flask, jsonify, request, render_template, make_response
 from util import json_utils
+from flask_swagger_ui import get_swaggerui_blueprint
+from flasgger import Swagger
+from flasgger.utils import swag_from
+from flasgger import LazyString, LazyJSONEncoder
 
 import flask_monitoringdashboard as dashboard
 import json
 import logging
 
 app = Flask(__name__)
+
+
+app.config["SWAGGER"] = {"title": "GAU-ML-UI", "uiversion": 2}
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec_1",
+            "route": "/apispec_1.json",
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/swagger/"
+}
+
+template = dict(
+    swaggerUiPrefix=LazyString(lambda: request.environ.get("HTTP_X_SCRIPT_NAME", ""))
+)
+
+app.json_encoder = LazyJSONEncoder
+swagger = Swagger(app, config=swagger_config, template=template)
+
+
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 dashboard.bind(app)
 
@@ -24,6 +56,26 @@ team = [
     }
 ]
 
+
+def add_2_numbers(num1, num2):
+    output = {"sum_of_numbers": 0}
+    sum_of_2_numbers = num1 + num2
+    output["sum_of_numbers"] = sum_of_2_numbers
+    return output
+
+
+@app.route("/add_2_numbers", methods=["POST"])
+@swag_from("static/swagger_config.yml")
+def add_numbers():
+    input_json = request.get_json()
+    try:
+        num1 = int(input_json["x1"])
+        num2 = int(input_json["x2"])
+        res = add_2_numbers(num1, num2)
+    except:
+        res = {"success": False, "message": "Unknown error"}
+
+    return json.dumps(res)
 
 @app.route("/is_valid", methods=['POST'])
 def first_hello_world():
@@ -74,3 +126,6 @@ def not_found(error):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+    #app.run(host='localhost')
+
+
